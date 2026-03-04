917f506e4ea7dce3ce74e305461e5957c8f86e48
Sehaj <2002iamsehaj@gmail.com>
2026-03-04 22:45:38 +0530

    Create permute.java

---
+++ permute.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[] used = new boolean [nums.length];
+        backtrack(nums, result, new ArrayList<>(), used);
+        return result;
+    }
+    private void backtrack(int[] nums, List<List<Integer>> result, List<Integer> current, boolean[] used){
+        // base case
+        if(nums.length==current.size()){
+            result.add(new ArrayList<>(current));
+            return;
+        }
+         for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            used[i] = true;
+            current.add(nums[i]);
+            backtrack(nums, result, current, used);
+            // Backtrack
+            current.remove(current.size() - 1);
+            used[i] = false;
+        }
+    }
+}
