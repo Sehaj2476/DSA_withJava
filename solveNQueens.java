f851285a97fc49161110e7c9ed6ea3ec40d2b8be
Sehaj <2002iamsehaj@gmail.com>
2026-04-02 23:13:51 +0530

    Create solveNQueens.java

---
+++ solveNQueens.java
@@ -0,0 +1,56 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        char board[][] = new char [n][n];
+        List<List<String>> result = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='.';
+            }
+        }
+        nQueens(board,0,result);
+        return result;
+    }
+    private void nQueens(char board[][], int row, List<List<String>> result){
+        // base 
+        if(row == board.length){
+              result.add(saveBoard(board));
+            return;
+        }
+        // column loop
+        for(int j = 0; j < board.length; j++){
+            if(isSafe(board, row, j)){
+                board[row][j] = 'Q';
+                nQueens(board, row + 1, result);
+                board[row][j] = '.';
+            }
+        }
+    }
+  private List<String> saveBoard(char[][] board) {
+        List<String> temp = new ArrayList<>();
+        for (int i = 0; i < board.length; i++) {
+            temp.add(new String(board[i]));
+        }
+        return temp;
+    }
+    private boolean isSafe(char board[][], int row, int col){
+        // vertical up
+        for(int i=row-1;i>=0;i--){
+            if(board[i][col]=='Q'){
+                return false;
+            }
+        }
+        // diagonal left up
+        for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        // diagonal right up
+        for(int i=row-1,j=col+1;i>=0&&j<board.length;i--,j++){
+            if(board[i][j]=='Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+}
